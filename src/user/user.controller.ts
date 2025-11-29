3ccaf1b56d3233e78a62cfbec8512fd6c59efad5
MielomankA <mielomanka@mail.ru>
2025-11-30 02:01:02 +0300

    feat: add user methods

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  HttpCode,
+  Post,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { ParseUUIDPipe } from '@nestjs/common/pipes';
 import { UserService } from './user.service';
+import { CreateUserDto } from 'src/dto/create-user.dto';
+import { UpdatePasswordDto } from 'src/dto/update-password.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
+  @HttpCode(200)
   getAll() {
     return this.userService.findAll();
   }
@@ -14,5 +26,24 @@ export class UserController {
   @Get(':id')
   getById(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
     return this.userService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() dto: CreateUserDto) {
+    return this.userService.create(dto);
+  }
+
+  @Put(':id')
+  updatePassword(
+    @Param('id', new ParseUUIDPipe({ version: '4' })) id: string,
+    @Body() dto: UpdatePasswordDto,
+  ) {
+    return this.userService.updatePassword(id, dto);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  delete(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.userService.delete(id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,37 +1,92 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  ForbiddenException,
+  BadRequestException,
+} from '@nestjs/common';
 import { User } from 'src/types/user';
-import crypto from 'node:crypto';
+import { randomUUID } from 'node:crypto';
+import { CreateUserDto } from 'src/dto/create-user.dto';
+import { UpdatePasswordDto } from 'src/dto/update-password.dto';
 
 @Injectable()
 export class UserService {
   private users: User[] = [];
 
-  findAll(): User[] {
-    return this.users;
+  private hidePassword(user: User) {
+    return {
+      id: user.id,
+      login: user.login,
+      version: user.version,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    };
   }
 
-  findOne(id: string): User {
-    const user = this.users.find((user) => user.id === id);
+  findAll() {
+    return this.users.map((user) => this.hidePassword(user));
+  }
 
+  findOne(id: string) {
+    const user = this.users.find((user) => user.id === id);
     if (!user) {
       throw new NotFoundException(`User with ID "${id}" not found`);
     }
-
-    return user;
+    return this.hidePassword(user);
   }
 
-  create(login: string, password: string): User {
-    const id = crypto.randomUUID();
-    const user: User = {
-      id,
-      login,
-      password,
+  create(dto: CreateUserDto) {
+    if (!dto.login || !dto.password) {
+      throw new BadRequestException(
+        'Missing required fields: login and password',
+      );
+    }
+
+    const timestamp = Date.now();
+
+    const newUser: User = {
+      id: randomUUID(),
+      login: dto.login,
+      password: dto.password,
       version: 1,
-      createdAt: Date.now(),
-      updatedAt: Date.now(),
+      createdAt: timestamp,
+      updatedAt: timestamp,
     };
 
-    this.users.push(user);
-    return user;
+    this.users.push(newUser);
+
+    return this.hidePassword(newUser);
+  }
+
+  updatePassword(id: string, dto: UpdatePasswordDto) {
+    if (!dto.oldPassword || !dto.newPassword) {
+      throw new BadRequestException('Missing oldPassword or newPassword');
+    }
+
+    const user = this.users.find((user) => user.id === id);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    if (user.password !== dto.oldPassword) {
+      throw new ForbiddenException('Old password is wrong');
+    }
+
+    user.password = dto.newPassword;
+    user.version += 1;
+    user.updatedAt = Date.now();
+
+    return this.hidePassword(user);
+  }
+
+  delete(id: string) {
+    const index = this.users.findIndex((user) => user.id === id);
+
+    if (index === -1) {
+      throw new NotFoundException('User not found');
+    }
+
+    this.users.splice(index, 1);
   }
 }
