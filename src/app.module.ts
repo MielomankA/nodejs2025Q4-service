8eac228f51af104a896cca58d47e485d76088a8b
MielomankA <mielomanka@mail.ru>
2025-12-01 00:09:31 +0300

    feat: generate album module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ArtistsModule } from './artists/artists.module';
 import { TracksModule } from './tracks/tracks.module';
+import { AlbumController } from './album/album.controller';
+import { AlbumModule } from './album/album.module';
 
 @Module({
-  imports: [UserModule, ArtistsModule, TracksModule],
-  controllers: [AppController],
+  imports: [UserModule, ArtistsModule, TracksModule, AlbumModule],
+  controllers: [AppController, AlbumController],
   providers: [AppService],
 })
 export class AppModule {}
