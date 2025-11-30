c452d370b4d574ba37b9dab8d26ca95d760b829d
MielomankA <mielomanka@mail.ru>
2025-11-30 23:04:06 +0300

    feat: add track controller/service

--- src/tracks/tracks.controller.ts
+++ src/tracks/tracks.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  Delete,
+  Body,
+  ParseUUIDPipe,
+} from '@nestjs/common';
+import { TracksService } from './tracks.service';
+import { CreateTrackDto } from 'src/dto/create-track.dto';
 
 @Controller('track')
-export class TracksController {}
+export class TracksController {
+  constructor(private readonly tracksService: TracksService) {}
+
+  @Get()
+  getAll() {
+    return this.tracksService.findAll();
+  }
+
+  @Get(':id')
+  getById(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.tracksService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() dto: CreateTrackDto) {
+    return this.tracksService.create(dto);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', new ParseUUIDPipe({ version: '4' })) id: string,
+    @Body() dto: CreateTrackDto,
+  ) {
+    return this.tracksService.update(id, dto);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  delete(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.tracksService.delete(id);
+  }
+}
--- src/tracks/tracks.service.ts
+++ src/tracks/tracks.service.ts
@@ -1,4 +1,58 @@
 import { Injectable } from '@nestjs/common';
+import { CreateTrackDto } from 'src/dto/create-track.dto';
+import { Track } from 'src/types/track';
+import { NotFoundException } from '@nestjs/common';
+import { randomUUID } from 'node:crypto';
 
 @Injectable()
-export class TracksService {}
+export class TracksService {
+  private tracks: Track[] = [];
+
+  findAll() {
+    return this.tracks;
+  }
+
+  findOne(id: string) {
+    const track = this.tracks.find((track) => track.id === id);
+
+    if (!track) {
+      throw new NotFoundException('Track not found');
+    }
+
+    return track;
+  }
+
+  create(dto: CreateTrackDto) {
+    const track: Track = {
+      id: randomUUID(),
+      name: dto.name,
+      artistId: dto.artistId ?? null,
+      albumId: dto.albumId ?? null,
+      duration: dto.duration,
+    };
+
+    this.tracks.push(track);
+    return track;
+  }
+
+  update(id: string, dto: CreateTrackDto) {
+    const track = this.findOne(id);
+
+    track.name = dto.name;
+    track.artistId = dto.artistId ?? null;
+    track.albumId = dto.albumId ?? null;
+    track.duration = dto.duration;
+
+    return track;
+  }
+
+  delete(id: string) {
+    const index = this.tracks.findIndex((track) => track.id === id);
+
+    if (index === -1) {
+      throw new NotFoundException('Track not found');
+    }
+
+    this.tracks.splice(index, 1);
+  }
+}
